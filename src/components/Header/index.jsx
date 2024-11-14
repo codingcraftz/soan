69ca231587cb83308d219174d5d9d95578fea702
zoonyoung <9851248@gmail.com>
2024-11-14 16:53:32 +0900

    refactor: Modify icon & font

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,38 @@
+// src/components/Header/index.jsx
+
 "use client";
 
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-6 border-b border-gray-200">
+    <header className="flex items-center justify-between p-6 border-b border-gray-200 font-quicksand">
       <Link href="/">
-        <h1 className="text-2xl font-semibold">정소안</h1>
+        <h1 className="text-2xl font-semibold font-korean">정소안</h1>
       </Link>
       <nav className="flex space-x-4">
-        <Link href="/music" className="text-gray-500 hover:text-gray-800">
+        <Link
+          href="/music"
+          className="text-gray-500 hover:text-gray-800 font-quicksand"
+        >
           Music
         </Link>
-        <Link href="/news" className="text-gray-500 hover:text-gray-800">
+        <Link
+          href="/news"
+          className="text-gray-500 hover:text-gray-800 font-quicksand"
+        >
           News
         </Link>
-        <Link href="/schedule" className="text-gray-500 hover:text-gray-800">
+        <Link
+          href="/schedule"
+          className="text-gray-500 hover:text-gray-800 font-quicksand"
+        >
           Schedule
         </Link>
-        <Link href="/contact" className="text-gray-500 hover:text-gray-800">
+        <Link
+          href="/contact"
+          className="text-gray-500 hover:text-gray-800 font-quicksand"
+        >
           Contact
         </Link>
       </nav>
