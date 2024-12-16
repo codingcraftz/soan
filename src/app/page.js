3ed9c73ff9b9da06a4163d67192d151d7f3b633f
zoonyoung <9851248@gmail.com>
2024-12-16 18:55:43 +0900

    refactor: Modify 모바일 반응형

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <main className="bg-gray-100 min-h-screen text-gray-800 font-sans w-full">
       {/* 반응형 레이아웃 */}
-      <section className="flex flex-col md:grid md:grid-cols-2 gap-8 p-8">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 p-8">
         {/* Albums */}
-        <div className="flex flex-col justify-center items-center text-center">
+        <div className="flex flex-col justify-center items-center text-center order-1">
           <h2 className="text-3xl font-semibold mb-4">Albums</h2>
           <p className="mb-6 text-gray-600">음악과 함께 떠나는 작은 여행</p>
           <Link
@@ -21,7 +21,7 @@ export default function Home() {
         </div>
 
         {/* 첫 번째 이미지 */}
-        <div className="flex items-center justify-center overflow-hidden">
+        <div className="flex items-center justify-center overflow-hidden order-2">
           <Image
             src="/images/home_1.jpg"
             alt="soan"
@@ -31,19 +31,8 @@ export default function Home() {
           />
         </div>
 
-        {/* News */}
-        <div className="flex flex-col justify-center items-center text-center">
-          <h2 className="text-3xl font-semibold mb-4">News</h2>
-          <p className="mb-6 text-gray-600">
-            공연 일정, 앨범 발매 소식 등 다양한 소식
-          </p>
-          <button className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-600">
-            우리의 이야기 보러가기
-          </button>
-        </div>
-
         {/* 두 번째 이미지 */}
-        <div className="flex items-center justify-center overflow-hidden">
+        <div className="flex items-center justify-center overflow-hidden order-4 md:order-3">
           <Image
             src="/images/home_2.jpg"
             alt="soan"
@@ -51,6 +40,17 @@ export default function Home() {
             width={300}
             height={300}
           />
+        </div>
+
+        {/* News */}
+        <div className="flex flex-col justify-center items-center text-center order-3 md:order-4">
+          <h2 className="text-3xl font-semibold mb-4">News</h2>
+          <p className="mb-6 text-gray-600">
+            공연 일정, 앨범 발매 소식 등 다양한 소식
+          </p>
+          <button className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-600">
+            우리의 이야기 보러가기
+          </button>
         </div>
       </section>
     </main>
